095e0d6c3b8e8f84a5b25c9a6527202b3cd497a0
Spencer Shaw <sfshaw@calpoly.edu>
2018-06-02 16:18:46 -0700

    Add loop to sockets test server

--- tests/sockets_test/server/sockets_server.py
+++ tests/sockets_test/server/sockets_server.py
@@ -6,26 +6,27 @@ PORT = 5000 # Arbitrary non-privileged port
 
 # create socket, use ipv4 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#s.setsockopt(socket.SOL_SOCKET, 25, 'eth0')
 print('Socket created')
  
 try:
     s.bind((HOST, PORT))
-except socket.error:
-    print('Bind failed. Error Code: ' + socket.error)
+except socket.error as socket_error:
+    print('Bind failed. Error Code: '.format(socket_error))
     s.close()
     sys.exit()
      
-print('Socket bind complete')
+print('Socket bind to ' + str(HOST) + ':' + str(PORT) + ' sucessful')
  
 s.listen(10)
 print('Socket now listening')
+
+#wait to accept a connection - blocking call
+conn, addr = s.accept()
+print('Connected with ' + addr[0] + ':' + str(addr[1]))
  
 #now keep talking with the client
 while 1:
-    #wait to accept a connection - blocking call
-    conn, addr = s.accept()
-    print('Connected with ' + addr[0] + ':' + str(addr[1]))
-     
     data = conn.recv(1024)
     reply = 'OK...' + str(data)
     if not data: 
@@ -36,3 +37,4 @@ while 1:
  
 conn.close()
 s.close()
+print('Socket closed')
