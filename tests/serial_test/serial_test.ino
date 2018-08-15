7d2b44e930b2c4e2e06669eeb4bd7b77d12ad446
Spencer Shaw <sfshaw@calpoly.edu>
2018-08-14 23:02:30 -0700

    Add initial serial test

--- tests/serial_test/serial_test.ino
+++ tests/serial_test/serial_test.ino
@@ -1,10 +1,27 @@
+#define INIT_CHAR '0'
+
+char received = 0;
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  while (!Serial) {
+
+  }
+  establishContact();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+}
+
+void establishContact() {
+  while (Serial.available() <= 0) {}
+  received = Serial.readBytes(&received, 1);
+  if (received == INIT_CHAR) {
+    Serial.print(INIT_CHAR);   // send a capital A
+    delay(300);
+  }
 }
 
