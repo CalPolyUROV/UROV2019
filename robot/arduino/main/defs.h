07adf88e87443bce16f4097a13dfc5a156eb74da
Spencer Shaw <sfshaw@calpoly.edu>
2018-08-18 13:05:38 -0700

    Add defs.h file

---
+++ robot/arduino/main/defs.h
@@ -0,0 +1,24 @@
+// defs
+
+#define USB_SERIAL 1
+#define SERIAL_2 2
+
+#define CHKSUM_MASK B00001111
+#define MAX_SEQNUM B00001111
+#define FIRST_SEQNUM B00000000
+
+//LUT
+// lookup table for indexes of commands
+// TODO: move to another file
+// byte cmd_lut[255] = {EST_CMD, SET_MOT_CMD, READ...}
+// currently using switch statement and bodge LUT, should still be moved to other file
+#define EST_CON_CMD 0x00
+#define SET_MOT_CMD 0x20
+#define RD_SENS_CMD 0x40
+
+struct packet {
+  byte cmd;
+  byte value1, value2;
+  byte seqnum_chksum; // first 4 bits used for sequence number, second 4 used for checksum
+};
+
--- robot/arduino/main/main.ino
+++ robot/arduino/main/main.ino
@@ -6,31 +6,8 @@
 
 */
 
-// defs
-// TODO: move to defs.h
-#define USB_SERIAL 1
-#define SERIAL_2 2
-
-#define CHKSUM_MASK B00001111
-#define MAX_SEQNUM B00001111
-#define FIRST_SEQNUM B00000000
-
-//LUT
-// lookup table for indexes of commands
-// TODO: move to another file
-// byte cmd_lut[255] = {EST_CMD, SET_MOT_CMD, READ...}
-// currently using switch statement and bodge LUT, should still be moved to other file
-#define EST_CON_CMD 0x00
-#define SET_MOT_CMD 0x20
-#define RD_SENS_CMD 0x40
-
-struct packet {
-  byte cmd;
-  byte value1, value2;
-  byte seqnum_chksum; // first 4 bits used for sequence number, second 4 used for checksum
-};
-
 #include "settings.h"
+#include "defs.h"
 usb_serial_class *coms_serial;
 // TODO: add debug serial port and utilize
 // HardwareSerial *debug_serial;
