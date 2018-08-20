e075b03b63b0651c4ed5310ee9573d10b8406865
Spencer Shaw <sfshaw@calpoly.edu>
2018-08-19 18:10:28 -0700

    Move list of ommands to packet.h

--- robot/arduino/main/defs.h
+++ robot/arduino/main/defs.h
@@ -23,12 +23,5 @@
 #define MAX_SEQNUM B00001111
 #define FIRST_SEQNUM B00000000
 
-//LUT
-// lookup table for indexes of commands
-// byte cmd_lut[255] = {EST_CMD, SET_MOT_CMD, READ...}
-// currently using switch statement and bodge LUT
-#define EST_CON_CMD 0x00
-#define SET_MOT_CMD 0x20
-#define RD_SENS_CMD 0x40
 #endif
 
--- robot/arduino/main/packet.h
+++ robot/arduino/main/packet.h
@@ -3,7 +3,17 @@
 
 #include "defs.h"
 
-#define PACKET_LENGTH 4 
+//LUT
+// lookup table for indexes of commands
+// byte cmd_lut[255] = {EST_CMD, SET_MOT_CMD, READ...}
+// currently using switch statement and bodge LUT
+#define EST_CON_CMD 0x00 // establish connection (command)
+#define SET_MOT_CMD 0x20 // set motor (command)
+#define MOT_SET_CMD 0x21 // motor has been set (reponse)
+#define RD_SENS_CMD 0x40
+
+
+#define PACKET_LENGTH 4
 // use for checking to see if buffer contains a packet
 
 struct packet {
