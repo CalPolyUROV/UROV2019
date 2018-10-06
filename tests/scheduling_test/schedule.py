705b11c9826247558c109daf7e360ef40306295f
sfshaw-calpoly <sfshaw@calpoly.edu>
2018-10-06 15:57:06 -0700

    Add initial scheduling code

--- tests/scheduling_test/schedule.py
+++ tests/scheduling_test/schedule.py
@@ -1,2 +1,21 @@
 # Scheduling class for scheduling activities
 
+from sockets_client import SocketsClient
+
+class Schedule:
+    def __init__(self):
+        self.task_list = [] # Empty list
+        self.socket_connection = SocketsClient() # Make sockets client obect
+
+    def get_new_tasks(self):
+        # communicate over sockets to generate new tasks based on UI input
+        self.socket_connection.send_data("Give me tasks")
+        pass
+
+    # Report whether there are enough tasks left in the queue
+    def has_tasks(self):
+        return len(self.task_list) 
+
+    # take the next task off the queue 
+    def get_next_task(self):
+        return self.task_list.pop(0)
