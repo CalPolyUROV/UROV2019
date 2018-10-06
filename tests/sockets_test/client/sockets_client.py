8773622394a619064204d4258f27b27db3046480
sfshaw-calpoly <sfshaw@calpoly.edu>
2018-10-06 12:48:44 -0700

    track blocking calls
    prepping for scheduling stuff

--- tests/sockets_test/client/sockets_client.py
+++ tests/sockets_test/client/sockets_client.py
@@ -45,7 +45,8 @@ while 1:
      
     print('Message send successfully')
      
-    #Now receive data
+    # Now receive data
+    # BLocking call?
     reply = s.recv(4096)
      
     print(reply)
