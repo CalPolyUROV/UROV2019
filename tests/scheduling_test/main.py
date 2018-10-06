eff5f59a8ed00a22d6cdfffee2d15d83e828ee51
sfshaw-calpoly <sfshaw@calpoly.edu>
2018-10-06 15:57:24 -0700

    Add scheduling test main

--- tests/scheduling_test/main.py
+++ tests/scheduling_test/main.py
@@ -1,2 +1,15 @@
 # Test code for on-robot scheduling system
 
+from schedule import Schedule
+
+# Make a schedule object
+s = Schedule()
+
+# loop()
+while(True):
+    # Get tasks if needed
+    if (not s.has_tasks()):
+        s.get_new_tasks()
+        
+    # Get the next task to execute
+    current_task = s.get_next_task()
