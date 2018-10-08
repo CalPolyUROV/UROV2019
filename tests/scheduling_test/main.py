8f13e0659ef7e1a0bf01a5ab14b8f964021fe2b7
sfshaw-calpoly <sfshaw@calpoly.edu>
2018-10-07 22:13:19 -0700

    Update test scheduling main.py

--- tests/scheduling_test/main.py
+++ tests/scheduling_test/main.py
@@ -1,6 +1,8 @@
 # Test code for on-robot scheduling system
 
 from schedule import Schedule
+from schedule import Task
+from time import sleep
 
 # Make a schedule object
 s = Schedule()
@@ -12,4 +14,8 @@ while(True):
         s.get_new_tasks()
         
     # Get the next task to execute
-    current_task = s.get_next_task()
+    t = s.get_next_task()
+    t.execute_task()
+    #sleep(2)
+    
+s.socket_connection.close_socket()
