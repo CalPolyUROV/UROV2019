9680574d43fbd3db35f9226f13e5ee66d371cc34
sfshaw-calpoly <sfshaw@calpoly.edu>
2018-10-11 19:35:01 -0700

    Implement channel selective debug()

--- robot/raspi/debug.py
+++ robot/raspi/debug.py
@@ -10,11 +10,14 @@ LOGGING = False
 #     debug("channel", "message with brackets: {}, {}".format("list", of_things_to_format_in)
 # By formatting once inside debug_f(), format() is only called if printing is turned on.
 
+channels = {"sockets": True,
+            "serial": True,
+            "execute_task": True}
+
 def debug(channel: str, message: str):
-    if(PRINTING):
+    if(PRINTING & channels[channel]):
         # Print message to console
         # TODO: Replace all print statements with calls to debug()
-        # TODO: Only print if a specific channel is set to be verbose
         print("{}: {}".format(channel, message))
     if(LOGGING):
         # TODO: Output stuff to a log file
