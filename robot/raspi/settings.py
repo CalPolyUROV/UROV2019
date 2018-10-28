1ce485ff217aeeac3d356bd713199b13dc5d3226
sfshaw-calpoly <sfshaw@calpoly.edu>
2018-10-27 19:23:46 -0700

    internal testing settings

--- robot/raspi/settings.py
+++ robot/raspi/settings.py
@@ -8,11 +8,12 @@ CHANNELS = {"schedule": True,
             "execute_task": True,
             "sockets": True,
             "socket_con": True,
-            "encode": True,
-            "decode": True,
+            "encode": False,
+            "decode": False,
             "serial_finder": True,
             "serial": True,
-            "serial_con":True}
+            "serial_con":True,
+            "packet":True}
 
 
 # Sockets Connection
@@ -20,15 +21,15 @@ USE_SOCKETS = True
 REQUIRE_SOCKETS = True
 TOPSIDE_IP_ADDRESS = "localhost" # '192.168.137.127'
 TOPSIDE_PORT = 9120
-ROBOT_IPADDRESS =  "192.168.137.50"
+ROBOT_IPADDRESS =  "localhost" # "192.168.137.50"
 SOCKETS_MAX_ATTEMPTS = 2  # Maximum number of times to try creating or opening a socket
 SOCKETS_RETRY_WAIT = 1  # seconds to wait before retrying sockets connection
 MAX_SOCKET_SIZE = 1024  # Maximum size for single receiving call
 
 
 # Serial Connection
-USE_SERIAL = False
-REQUIRE_SERIAL = False
+USE_SERIAL = True
+REQUIRE_SERIAL = True
 SERIAL_BAUD = 9600  # Serial Baudrate
 SERIAL_MAX_ATTEMPTS = 2  # Maximum number of times to try openeing a serial port
 
