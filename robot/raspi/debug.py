e95a70f0bc689ab990e7dcd8279f66af85c32838
Spencer Shaw <sfshaw@calpoly.edu>
2018-11-25 11:17:02 -0800

    Multithread debugging calls

--- robot/raspi/debug.py
+++ robot/raspi/debug.py
@@ -1,6 +1,7 @@
 """Debugging print and logging functions
 """
 import settings
+import _thread
 
 """Usage:
 debug("channel", "message")
@@ -16,7 +17,17 @@ By formatting once inside debug_f(), format() is only called if printing is turn
 
 
 def debug(channel: str, message: str):
-    if(settings.PRINTING & settings.CHANNELS[channel]):
+    if(settings.CHANNELS[channel]):
+        _thread.start_new_thread(_seq_debug, (channel, message))
+
+
+def debug_f(channel: str, message: str, formatting: list):
+    if(settings.CHANNELS[channel]):
+        _thread.start_new_thread(seq_debug_f, (channel, message, formatting))
+
+
+def _seq_debug(channel: str, message: str):
+    if(settings.PRINTING):
         # Print message to console
         print("{}:\t{}".format(channel, message))
     if(settings.LOGGING):
@@ -24,8 +35,8 @@ def debug(channel: str, message: str):
         pass
 
 
-def debug_f(channel: str, message: str, formatting: list):
-    if(settings.PRINTING & settings.CHANNELS[channel]):
+def seq_debug_f(channel: str, message: str, formatting: list):
+    if(settings.PRINTING):
         # TODO: Do this better
         print("{}:\t{}".format(channel, message.format(*formatting)))
     if(settings.LOGGING):
