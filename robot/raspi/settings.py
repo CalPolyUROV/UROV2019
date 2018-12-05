43a46da801cded84c3695b7864195c5d2fc2ec75
Spencer Shaw <sfshaw@calpoly.edu>
2018-12-04 22:55:48 -0800

    Update settings for server testing

--- robot/raspi/settings.py
+++ robot/raspi/settings.py
@@ -1,39 +1,37 @@
 """Configurable settings that apply to the operation of the robot
 """
 
-ROLE = "not set"  # Not a user facing setting
-
 # Debug channels
 CHANNELS = {"schedule": True,
             "execute_task": True,
-            
-            "sockets": False,
-            "socket_con": False,
-            "encode": False,
-            "decode": False,
+
+            "sockets": True,
+            "socket_con": True,
+            "encode": True,
+            "decode": True,
 
             "serial_finder": False,
             "serial": True,
-            "serial_con":True,
-            "ser_packet":True,
-            "chksum":False,
-            
-            "test":False}
+            "serial_con": True,
+            "ser_packet": True,
+            "chksum": False,
+
+            "test": True}
 
 
 # Sockets Connection
-USE_SOCKETS = False
+USE_SOCKETS = True
 REQUIRE_SOCKETS = False
-TOPSIDE_IP_ADDRESS = "localhost" # '192.168.137.127'
+TOPSIDE_IP_ADDRESS = "localhost"  # '192.168.137.127'
 TOPSIDE_PORT = 9120
-ROBOT_IPADDRESS =  "localhost" # "192.168.137.50"
+ROBOT_IPADDRESS = "localhost"  # "192.168.137.50"
 SOCKETS_MAX_ATTEMPTS = 2  # Maximum number of times to try creating or opening a socket
 SOCKETS_RETRY_WAIT = 1  # seconds to wait before retrying sockets connection
 MAX_SOCKET_SIZE = 1024  # Maximum size for single receiving call
 
 
 # Serial Connection
-USE_SERIAL = True
+USE_SERIAL = False
 REQUIRE_SERIAL = True
 SERIAL_BAUD = 19200  # Serial Baudrate
 SERIAL_MAX_ATTEMPTS = 4  # Maximum number of times to try openeing a serial port
@@ -43,3 +41,10 @@ SERIAL_MAX_ATTEMPTS = 4  # Maximum number of times to try openeing a serial port
 # TODO: track debugging for server and client separately
 PRINTING = True
 LOGGING = False
+
+# Robot
+ROBOT_NAME = "Subrina"
+
+# -----Do NOT change anything below this line while trying to Configurable-----
+
+ROLE = "not set"  # Not a user facing setting
