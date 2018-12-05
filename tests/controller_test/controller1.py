5a6a70450d5f6c7fb1ac58c86b0a14d9440b0a50
Spencer Shaw <sfshaw@calpoly.edu>
2018-12-04 22:52:40 -0800

    Complete controller test

--- tests/controller_test/controller1.py
+++ tests/controller_test/controller1.py
@@ -4,6 +4,30 @@
 import pygame
 import _thread
 
+# Mapping of pygame joystick output to values we can make sense of
+# "pygame_name":"name_we_use",
+control_mappings = {"number": "controller_num",
+                    "name": "controler_name",
+                    "axis_0": "axis_0",
+                    "axis_1": "axis_1",
+                    "axis_2": "axis_2",
+                    "axis_3": "axis_3",
+                    "axis_4": "axis_4",
+                    "axis_5": "axis_5",
+                    "axis_1": "axis_1",
+                    "button_0": "button_a",
+                    "button_1": "button_b",
+                    "button_2": "button_x",
+                    "button_3": "button_y",
+                    "button_4": "button_4",
+                    "button_5": "button_5",
+                    "button_6": "button_6",
+                    "button_7": "button_7",
+                    "button_8": "button_8",
+                    "button_9": "button_9",
+                    "dpad": "dpad"
+                    }
+
 
 class Controller:
 
@@ -24,51 +48,27 @@ class Controller:
 
     def map_data(self, joystick_data: dict) -> dict:
         control_data = {}
-        control_data["controller_num"] = self.try_key(joystick_data, "number")
-        control_data["controller_name"] = self.try_key(joystick_data, "name")
-        control_data["axis_1"] = self.try_key(joystick_data, "axis_1")
-        control_data["axis_2"] = self.try_key(joystick_data, "axis_2")
-        control_data["axis_3"] = self.try_key(joystick_data, "axis_3")
-        control_data["axis_4"] = self.try_key(joystick_data, "axis_4")
-        control_data["button_a"] = self.try_key(joystick_data, "button_0")
-        control_data["button_b"] = self.try_key(joystick_data, "button_1")
-        control_data["button_x"] = self.try_key(joystick_data, "button_2")
-        control_data["button_y"] = self.try_key(joystick_data, "button_3")
-        control_data["button_4"] = self.try_key(joystick_data, "button_4")
-        control_data["button_5"] = self.try_key(joystick_data, "button_5")
-        control_data["button_6"] = self.try_key(joystick_data, "button_6")
-        control_data["button_7"] = self.try_key(joystick_data, "button_7")
-        control_data["button_8"] = self.try_key(joystick_data, "button_8")
-        control_data["button_9"] = self.try_key(joystick_data, "button_9")
-        control_data["dpad"] = self.try_key(joystick_data, "dpad")
+        for k in joystick_data:
+            control_data[self.try_key(control_mappings, k)] = joystick_data[k]
         return control_data
 
     def try_key(self, d: dict, k: str):
         try:
             return d[k]
         except (KeyError):
-            return "Key invalid: " + k
+            return "Key not supplied in mapping: " + k
 
     def initialize(self):
-
         pygame.init()
-
         # Loop until the user clicks the close button.
         done = False
-
-        # Used to manage how fast the screen updates
-        # clock = pygame.time.Clock()
-
         # Initialize the joysticks
         pygame.joystick.init()
-
-        # -------- Main Program Loop -----------
-        while done == False:
+        while not done:
             # EVENT PROCESSING STEP
             for event in pygame.event.get():  # User did something
                 if event.type == pygame.QUIT:  # If user clicked close
                     done = True  # Flag that we are done so we exit this loop
-
                 # Possible joystick actions: JOYAXISMOTION JOYBALLMOTION JOYBUTTONDOWN JOYBUTTONUP JOYHATMOTION
                 if event.type == pygame.JOYBUTTONDOWN:
                     # print("Joystick button pressed.")
@@ -84,37 +84,27 @@ class Controller:
             for i in range(joystick_count):
                 joystick = pygame.joystick.Joystick(i)
                 joystick.init()
-
                 # Get the index of which joystick in the event that multiple are connected
                 self.joystick_data["number"] = i
                 # Get the name from the OS for the controller/joystick
                 self.joystick_data["name"] = joystick.get_name()
-                self.joystick_data["num_axes"] = joystick.get_numaxes()
 
+                self.joystick_data["num_axes"] = joystick.get_numaxes()
                 for i in range(self.joystick_data["num_axes"]):
                     self.joystick_data["axis_" + str(i)] = joystick.get_axis(i)
 
                 self.joystick_data["num_buttons"] = joystick.get_numbuttons()
-
                 for i in range(self.joystick_data["num_buttons"]):
                     self.joystick_data["button_" +
                                        str(i)] = joystick.get_button(i)
-
                 # Hat switch. All or nothing for direction, not like joysticks.
                 # Value comes back in an array.
                 self.joystick_data["num_dpad"] = joystick.get_numhats()
                 for i in range(self.joystick_data["num_dpad"]):
                     self.joystick_data["dpad"] = joystick.get_hat(i)
 
-            # ALL CODE TO DRAW SHOULD GO ABOVE THIS COMMENT
-
-            # # Go ahead and update the screen with what we've drawn.
-            # pygame.display.flip()
-
-            # # Limit to 20 frames per second
-            # clock.tick(20)
-
-    # Close the window and quit.
-    # If you forget this line, the program will 'hang'
-    # on exit if running from IDLE.
-    pygame.quit()
+    def close():
+        # Close the window and quit.
+        # If you forget this line, the program will 'hang'
+        # on exit if running from IDLE.
+        pygame.quit()
