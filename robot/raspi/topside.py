0aca5004794a6a8de148275d06ac2c34d529950b
Spencer Shaw <sfshaw@calpoly.edu>
2018-12-04 22:54:37 -0800

    Rename server file and add controller

--- robot/raspi/topside.py
+++ robot/raspi/topside.py
@@ -7,21 +7,32 @@ import settings
 from utils import exit, debug, debug_f
 from task import Task, TaskType, TaskPriority
 from sockets_server import SocketsServer
-from server import handle_response
+from control_unit import ControlUnit
 
-settings.ROLE = "server"
 
-s = SocketsServer(settings.TOPSIDE_IP_ADDRESS, settings.TOPSIDE_PORT)
+def main():
+    settings.ROLE = "server"
 
-s.open_server()
+    cu = ControlUnit()
+    s = SocketsServer(settings.TOPSIDE_IP_ADDRESS, settings.TOPSIDE_PORT)
 
-task_queue = []
-handler = handle_response
+    s.open_server()
 
-while 1:
-    s.accept_connection()
+    task_queue = []
+    handler = cu.handle_response
+
     while 1:
-        s.recieve_data(task_queue, handler)
+        s.accept_connection()
+        while 1:
+            s.recieve_data(task_queue, handler)
 
-    s.conn.close()
-    s.close()
+        s.conn.close()
+        s.close()
+
+
+# https://stackoverflow.com/questions/21120947/catching-keyboardinterrupt-in-python-during-program-shutdown
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        exit()
