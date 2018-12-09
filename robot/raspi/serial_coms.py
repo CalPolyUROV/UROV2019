79a3a9dd06cd53ad33f714e0cf2650ef90f2e14c
Spencer Shaw <sfshaw@calpoly.edu>
2018-12-09 00:43:27 -0800

    Rename packet file

--- robot/raspi/serial_coms.py
+++ robot/raspi/serial_coms.py
@@ -12,14 +12,10 @@ import serial_finder  # Identifies serial ports
 import settings
 from utils import sleep, debug, debug_f
 from task import Task, TaskPriority, TaskType
+from serial_packet import Packet
 
 # encoding scheme
 ENCODING = 'ascii'
-
-# Bitmask for extracting checksums from seqnum_chksum
-# Do not use directly, implement a checksum verification method
-# TODO: verify checksums, probably in read_packet()
-CHKSUM_MASK = 0b00001111
 
 # Initial value for sequence number
 FIRST_SEQNUM = 0
@@ -41,90 +37,6 @@ INV_CMD_ACK = 0xFF  # Invalid command, value2 of response contains cmd
 # Magic numbers to verify correct initial packet and response
 EST_CON_VAL1 = 0b10100101
 EST_CON_VAL2 = 0b01011010
-
-
-def calc_chksum(cmd: int, val1: int, val2: int, seqnum: int) -> int:
-    sum = (cmd +
-           (val1 * 3) +
-           (val2 * 5) +
-           (seqnum * 7)) & CHKSUM_MASK
-    return sum
-    # idk, it has primes
-    # TODO: Make this better, but it must match on this and the Arduino/Teensy. (Maybe CRC32?)
-
-
-class Packet:
-    """ Packet class for storing information that is sent and received over serial
-    """
-
-    def __init__(self, cmd: int, val1: int, val2: int, seqnum_chksum: int):
-        """Internal constructor
-        """
-        self.cmd = cmd
-        self.val1 = val1
-        self.val2 = val2
-        self.seqnum_chksum = seqnum_chksum
-
-        # debug_f("ser_packet", "Constructed: {}", [self])
-
-    def extract_seqnum(self, seqnum_chksum: int) -> int:
-        return int.from_bytes(seqnum_chksum, byteorder='big') >> 4
-
-    def get_seqnum(self) -> int:
-        return self.seqnum_chksum >> 4
-
-    def extract_chksum(self, seqnum_chksum: int) -> int:
-        return seqnum_chksum & CHKSUM_MASK
-
-    def get_chksum(self) -> int:
-        return self.seqnum_chksum & CHKSUM_MASK
-
-    def isValid(self) -> bool:
-        chksum = self.get_chksum()
-        expected = calc_chksum(self.cmd, self.val1,
-                               self.val2, self.get_seqnum())
-        debug_f('chksum', "Packet had chksum of {}, {} was expected", [
-                chksum, expected])
-        return chksum == expected
-
-    def __eq__(self, other) -> bool:
-        return ((self.__class__ == other.__class__) and
-                (self.cmd == other.cmd) and
-                (self.val1 == other.val1) and
-                (self.val2 == other.val2) and
-                (self.seqnum_chksum == other.seqnum_chksum))
-
-    def __repr__(self):
-        return "Packet: cmd: {} val1: {} val2: {} seqnum: {} chksum: {}".format(self.cmd, self.val1, self.val2, self.get_seqnum(), self.get_chksum())
-
-
-def new_packet(cmd: int, val1: int, val2: int, seqnum: int):
-    """ Constructor for building packets to send (chksum is created)
-    """
-    chksum = calc_chksum(cmd, val1, val2, seqnum)
-    return Packet(cmd, val1, val2, ((seqnum << 4) + chksum))
-
-
-def make_packet(cmd: int, val1: int, val2: int, seqnum: int, chksum: int):
-    """ Constructor for building packets (chksum is given)
-    """
-    # chksum = calc_chksum(cmd, val1, val2, seqnum)
-    return Packet(cmd, val1, val2, ((seqnum << 4) + chksum))
-
-
-def parse_packet(cmd: bytes, val1: bytes, val2: bytes, seqnum_chksum: bytes):
-    """Constructor for building packets that have been received, untrusted checksums
-    """
-    _cmd = int.from_bytes(cmd, byteorder='big')
-    _val1 = int.from_bytes(val1, byteorder='big')
-    _val2 = int.from_bytes(val2, byteorder='big')
-    _seqnum = int.from_bytes(seqnum_chksum, byteorder='big') >> 4
-    _chksum = int.from_bytes(seqnum_chksum, byteorder='big') & CHKSUM_MASK
-    p = make_packet(_cmd, _val1, _val2, _seqnum, _chksum)
-    if(p.isValid()):
-        return p
-    else:
-        debug_f("ser_packet", "read invalid packet {}", [p])
 
 
 def find_port():
@@ -242,7 +154,7 @@ class SerialConnection:
 
     # Send the inital packet and wait for the correct response
     def establish_contact(self):
-        p_out = new_packet(EST_CON_CMD, EST_CON_VAL1, EST_CON_VAL2, FIRST_SEQNUM)
+        p_out = Packet.new_packet(EST_CON_CMD, EST_CON_VAL1, EST_CON_VAL2, FIRST_SEQNUM)
         debug_f('serial_con', "Establishing connection by sending {}", [p_out])
         # Send initial packet and receive response
         p_in = self.send_receive_packet(p_out)
@@ -258,3 +170,16 @@ class SerialConnection:
             # TODO: Add logic to retry this a few times
             t = Task(TaskType.serial_est_con, TaskPriority.high, [])
             return [t]
+
+
+    def new_packet(cmd: int, val1: int, val2: int, seqnum: int):
+        """ Constructor for building packets to send (chksum is created)
+        """
+        chksum = Packet.calc_chksum(cmd, val1, val2, seqnum)
+        return Packet(cmd, val1, val2, ((seqnum << 4) + chksum))
+
+    def make_packet(self, cmd: int, val1: int, val2: int, chksum: int):
+        """ Constructor for building packets (chksum is given)
+        """
+        # chksum = calc_chksum(cmd, val1, val2, seqnum)
+        return Packet(cmd, val1, val2, ((self.next_seqnum() << 4) + chksum))
