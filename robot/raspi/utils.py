826b4b510ada1b5968f052cd4be9bd9a33c5c32b
Spencer Shaw <sfshaw@calpoly.edu>
2019-01-26 16:53:24 -0800

    Update utils.py
    Add attempt()
    Clean up docs

--- robot/raspi/utils.py
+++ robot/raspi/utils.py
@@ -1,34 +1,38 @@
-"""Various helping utilities used throughout the code
-
+"""Various helper utilities used throughout the code
+Attempts to document propper usage of such functions
 """
 
 # System imports
 import os  # For exit
+import random  # Simulated control values
 import sys  # For exit
 import time  # For sleep
+from typing import Callable, NewType
 import _thread  # For  multi threaded debug
-import random  # Simulated control values
 
 # Our imports
 import settings
 
 
-def sleep(time_s: int):
-    """Pauses the execution of the thread
-    Do not use in production
+def sleep(time_s: int) -> None:
+    """Pauses the execution of the thread for time_s seconds
     """
     # TODO: prevent use of this via a setting
     time.sleep(time_s)
 
 
-def exit(reason: str):
+def exit(reason: str) -> None:
+    """Kills the program after printing the supplied str reason
+    """
     print("\nExiting: " + reason.__repr__())
     try:
         sys.exit(0)
     except SystemExit:
         os._exit(0)
+    # This point should be unreachable, just die already
 
-# TODO: Use settings.ROLE for per client and server debugging
+def debug_delay():
+    sleep(settings.DEBUGGING_DELAY_S)
 
 
 def debug(channel: str, *args):
@@ -50,9 +54,17 @@ def debug(channel: str, *args):
     channel: message with brackets: list, thing_to_format.__repr__()
 
     By formatting once inside debug(), format() is only called if printing is
-    turned on. Remember to include [ ] around the items to be formatted
+    turned on. Remember to include [ ] around the items to be formatted.
+
+    Note that one iteration of this code spawned a separte thread for every
+    debug() call. The printing system call could not keep up and threads
+    piled up and eventually crashed the program. Either threads must be 
+    managed with pools or print statements can be allowed to slow down the
+    program (They can be turn off in settings)
     """
-    if(settings.PRINTING and channel_active(channel)):
+
+    # TODO: Use settings.ROLE for per client and server debugging
+    if(settings.DEBUG_PRINTING and channel_active(channel)):
         n = len(args)
         # Print message to console
         if n == 1:
@@ -61,25 +73,17 @@ def debug(channel: str, *args):
             print("{}:\t{}".format(channel, args[0].format(*args[1])))
         elif 2 > 1:
             print("{}:\t{}".format(channel, args[0].format(*args[1:])))
-    if(settings.LOGGING and channel_active(channel)):
+    if(settings.DEBUG_LOGGING and channel_active(channel)):
         # TODO: Output stuff to a log file
+        def log(channel: str, message: str):
+            """Unimplemented logging function
+            """
+            # TODO: Add logging to a specific file
+            # (such as for logging the port that a sockets connection is on, so that the
+            #  value is accessible to the other Pi over a network share)
+            pass
+        debug("framework", "Logging to file is not implemented")
         pass
-        # Do NOT use printing on another thread because it is so slow
-        # that thread will pile up and the limit of number of threads will
-        #  be reached
-
-# Deprecated, use debug()
-# def debug_f(channel: str, message: str, formatting: list):
-#     if(settings.PRINTING and channel_active(channel)):
-#         # TODO: Do this better
-#         print("{}:\t{}".format(channel, message.format(*formatting)))
-#     if (settings.LOGGING and channel_active(channel)):
-#         # TODO: Output stuff to a log file
-#         pass
-#         # Do NOT use printing on another thread because it is so slow
-#         # that thread will pile up and the limit of number of threads will
-#         #  be reached
-#         # _thread.start_new_thread(log_f, (channel, message, formatting))
 
 
 def channel_active(channel: str) -> bool:
@@ -88,26 +92,19 @@ def channel_active(channel: str) -> bool:
     Channels that are not found are debugged by default
     """
     if channel in settings.DEBUG_CHANNELS:
-        return settings.DEBUG_CHANNELS[channel]
+        val = settings.DEBUG_CHANNELS[channel]
+        if isinstance(val, bool):
+            return val
+        else:
+            return  val < settings.DEBUG_LEVEL
     return True  # default for unknown channels
-
-# TODO: Add logging to a specific file
-# (such as for logging the port that a sockets connection is on, so that the
-#  value is accessible to the other Pi over a network share)
-
-
-def log(channel: str, message: str):
-    pass
-    # TODO: Implement logging to disk
-
-
-def log_f(channel: str, message: str, formatting: list):
-    pass
-    # TODO: Implement formatted logging to disk
 
 
 # Simulation tools
 def random_val():
+    """Generates random values for simulated control input
+    """
+    # TODO: Account for different kinds of input data such as joysticks vs buttons
     return random.randint(0, 1)
 
 
@@ -120,3 +117,41 @@ def try_key(d: dict, k: str):
         debug("controls_reader", "Unknown control key: ", [k])
         # TODO: Investigate changing this behavior
         return "Key not supplied in mapping: " + k
+
+
+Attemptable = NewType("Attemptable", Callable[[None], bool])
+
+
+def attempt(action: Attemptable, tries: int, fail_once: Callable, failure: Callable[[int], None]) -> None:
+    """Wrapper for trying to complete and action with a number of tries
+    Should follow this prototype:
+    def attmpt_action():
+        def try_action() -> bool:
+            try:
+                return True
+            except Exception as error:
+                debug("channel", "error: {}", [error.__repr__()])
+                return False
+
+        def fail_once() -> None:
+            debug("action", "action failed, retrying")
+            sleep(settings.ACTION_RETRY_WAIT)
+
+        def failure(tries: int) -> None:
+            if settings.REQUIRE_ACTION:
+                exit("Could not do required action")
+            else:
+                debug("action", "Ignoring failing action after {} tries", [tries])
+                settings.USE_ACTION = False
+
+        attempt(try_action, settings.ACTION_ATTEMPTS, fail_once, failure)
+        debug("action", "Did action")
+    """
+    attempts = 1
+    while (not action()):
+        if attempts >= tries:
+            failure(attempts)
+            return
+        else:
+            fail_once()
+            attempts += 1
