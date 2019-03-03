873073557a8001734820ac4426d566be0edecf0c
Spencer Shaw <sfshaw@calpoly.edu>
2019-03-03 13:44:08 -0800

    Use array size from settings and keep running sum

--- robot/raspi/internal_temp.py
+++ robot/raspi/internal_temp.py
@@ -9,7 +9,6 @@ from snr import Source
 from datastore import Datastore
 
 CMD = "vcgencmd measure_temp"
-ARRAY_SIZE = 4
 INVALID_VALUE = -2
 
 
@@ -35,31 +34,33 @@ class IntTempMon(Source):
     def __init__(self, name: str, store_data: Callable):
         super().__init__(name, self.monitor_temp, settings.INT_TEMP_MON_TICK_RATE)
         self.store_data = store_data
-        self.deque = deque(-1 for i in range(0, ARRAY_SIZE))
+        self.deque = deque()
+        self.sum = 0
 
         self.init_temp()
         self.loop()
 
     def init_temp(self):
-        for i in range(0, ARRAY_SIZE):
-            self.queue_reading(measure_temp())
+        """Prepare the monitor for use"""
+        for i in range(0, settings.INT_TEMP_MON_AVG_PERIOD):
+            self.deque.appendleft(i)
+            self.sum += i
 
     def monitor_temp(self):
+        """Main entry point called during loop"""
         self.queue_reading(measure_temp())
         avg = self.compute_avg()
-        self.store_data(self.name, avg)
+        self.store_data(self.name, avg)  # Send data to Node's datastore
         debug("int_temp_mon", "Temperature: {}'C", [avg])
 
     def queue_reading(self, new_val: int):
-        self.deque.pop()
+        """Place the temperature value in the queue and maintain the sum"""
+        self.sum -= self.deque.pop()
         self.deque.appendleft(new_val)
+        self.sum += new_val
 
     def compute_avg(self) -> int:
-        sum = 0
-        for i in self.deque:
-            if i is not None:
-                sum += i
-        return sum / ARRAY_SIZE
+        return self.sum / settings.INT_TEMP_MON_AVG_PERIOD
 
     def terminate(self):
         super().set_terminate_flag()
