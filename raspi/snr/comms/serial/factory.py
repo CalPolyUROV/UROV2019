92835fbcd6b2aaaac22fa0671aca33eaba1f0dd9
Spencer Shaw <sfshaw@calpoly.edu>
2020-01-13 13:25:34 -0800

    Protect main.py from pyserial imports

--- raspi/snr/comms/serial/factory.py
+++ raspi/snr/comms/serial/factory.py
@@ -1,4 +1,3 @@
-from snr.comms.serial.serial_connection import SerialConnection
 from snr.endpoint import Endpoint
 from snr.factory import Factory
 from snr.node import Node
@@ -10,9 +9,11 @@ class SerialFactory(Factory):
         super().__init__()
         self.transmit_data_name = transmit_data_name
         self.query_data_name = query_data_name
-        self.firmware_path = firmware_path  # Not used
+        # TODO: Support updating Arduino firmware on startup
+        # self.firmware_path = firmware_path
 
     def get(self, parent: Node) -> Endpoint:
+        from snr.comms.serial.serial_connection import SerialConnection
         return SerialConnection(parent, "Serial Connection",
                                 self.transmit_data_name,
                                 self.query_data_name)
