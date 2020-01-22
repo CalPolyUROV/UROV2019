651cab3cc00f4d7efa9aa23dccd07922eb88b9c3
Spencer Shaw <sfshaw@calpoly.edu>
2020-01-22 11:52:27 -0800

    Add shared c library

---
+++ raspi/snr/zynq/c_src/libdma.c
@@ -0,0 +1,7 @@
+int myFunction(int num)
+{
+    if (num == 0)
+        return 0;
+    else
+        return num * num;
+}
--- raspi/snr/zynq/zybo.py
+++ raspi/snr/zynq/zybo.py
@@ -1,12 +1,21 @@
+from ctypes import cdll, CDLL
+
 from snr.endpoint import Endpoint
 from snr.task import SomeTasks, Task
 from snr.node import Node
+from snr.utils import debug
 
 
 class Zybo(Endpoint):
     def __init__(self, parent: Node, name: str,
                  input: str, output: str):
         super().__init__(parent, name)
+
+        # https://docs.python.org/3/library/ctypes.html
+        lib_name = "/home/sfshaw/git/urov/raspi/libdma.so"
+        cdll.LoadLibrary(lib_name)
+        self.function = CDLL(lib_name)
+
         self.input = input
         self.output = output
 
@@ -15,8 +24,39 @@ class Zybo(Endpoint):
         return None
 
     def task_handler(self, t: Task) -> SomeTasks:
-        # TODO: Match serial task handling
-        return None
+        sched_list = []
+        if t.task_type == "serial_com":
+            debug("serial_verbose",
+                  "Executing serial com task: {}", [t.val_list])
+            cmd = t.val_list[0]
+            reg = t.val_list[1]
+            if len(t.val_list) > 2:
+                val = t.val_list[2]
+            else:
+                val = 0
+
+            result = self.dma_write(cmd, reg, val)
+
+            # result = self.send_receive(t.val_list[0],
+            #                            t.val_list[1::])
+            if result is None:
+                debug("robot",
+                      "Received no data in response from serial message")
+            elif isinstance(result, Task):
+                sched_list.append(result)
+            elif isinstance(result, list):
+                for new_task in list(result):
+                    sched_list.append(new_task)
+
+        return sched_list
+
+    def dma_write(self, cmd: str, reg: int, val: int):
+        debug("dma_verbose", "Writing DMA: cmd: {}, reg: {}, val: {}",
+              [cmd, reg, val])
+
+        status = self.function.myFunction(reg)
+
+        debug("dma_verbose", "cmd returned: {}", [status])
 
     def terminate(self):
         # No need to deconstruct/close in any weird way (yet)
