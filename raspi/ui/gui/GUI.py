2cdb4cac169351c819c4856bad43e095f41641f9
prcox <56206598+prcox@users.noreply.github.com>
2020-01-25 11:12:42 -0800

    Reformatted a bit

--- raspi/ui/gui/GUI.py
+++ raspi/ui/gui/GUI.py
@@ -23,34 +23,51 @@ class start_GUI(Endpoint):
         self.loop()
 
     def gui_print(self):
-        layout = [[sg.Text('Telemetry Data', size=(40, 2), justification='center')],
-                  [sg.Text('Stick Left X    Stick Right X    Left Trigger', size=(40, 2), justification='center')],
-                  [sg.Text('', size=(40, 2), font=('Helvetica', 10), justification='center', key='_0-2_')],
-                  [sg.Text('Stick Left Y    Stick Right Y    Right Trigger', size=(40, 2), justification='center')],
-                  [sg.Text('', size=(40, 2), font=('Helvetica', 10), justification='center', key='_3-5_')],
-                  [sg.Text("_" * 45)],
-                  [sg.Text('Refresh Rate', size=(40, 2), justification='center')],
-                  [sg.Text(' ' * 8), sg.Slider(range=(0, 50), default_value=47, size=(20, 20), orientation='horizontal',
+        layout = [# [sg.Text('Telemetry Data', size=(40, 2), justification='center')],
+                  # [sg.Text('Stick Left X    Stick Right X    Left Trigger', size=(40, 2), justification='center')],
+                  # [sg.Text('', size=(40, 2), font=('Helvetica', 10), justification='center', key='_0-2_')],
+                  # [sg.Text('Stick Left Y    Stick Right Y    Right Trigger', size=(40, 2), justification='center')],
+                  # [sg.Text('', size=(40, 2), font=('Helvetica', 10), justification='center', key='_3-5_')],
+                  # [sg.Text("_" * 45)],
+                  [sg.Frame(layout=[[sg.Text('Stick Left X: \n0\nStick Left Y: \n0\nLeft Trigger: \n0\n', size=(20, 6),
+                           justification='center', key='left'),
+                   # sg.RButton('', image_filename="test", size=(1, 1), pad=(0, 0), key='key'),
+                   sg.Text('Stick Right X: \n0\nStick Right Y: \n0\nRight Trigger: \n0\n', size=(20, 6),
+                           justification='center', key='right')]], title='Telem Data', title_color='Black', relief=sg.RELIEF_SUNKEN)],
+                  [sg.Text('Refresh Rate', size=(45, 2), justification='center')],
+                  [sg.Text(' ' * 13), sg.Slider(range=(0, 50), default_value=47, size=(20, 20), orientation='horizontal',
                                                font=("Helvetica", 15)), sg.Text(' ' * 2)],
-                  [sg.T(' ' * 32), sg.Quit()]]
+                  [sg.T(' ' * 38), sg.Quit()],
+                  # [sg.T('Change circle color to:'), sg.Button('Red'), sg.Button('Blue'), sg.Button('Move')]
+                  ]
 
         window = sg.Window('Top Side', layout)
 
         refresh_rate = 10
         telem = self.get_data()
-
+        # pick = window["graph"]
         while not self.terminate_flag:  # Event Loop
             event, values = window.Read(
                 timeout=refresh_rate)  # Please try and use as high of a timeout value as you can
             if event is None or event == 'Quit':  # if user closed the window using X or clicked Quit button
                 self.terminate()
             if telem is not None and telem.get("stick_left_x") is not None:
-                window.Element('_0-2_').Update(
-                    '{}            {}            {}'.format((telem.get("stick_left_x") // 1), (telem.get("stick_right_x") // 1),
-                                                  (telem.get("trigger_left") // 1)))
-                window.Element('_3-5_').Update(
-                    '{}            {}            {}'.format((telem["stick_left_y"] // 1), (telem["stick_right_y"] // 1),
-                                                  (telem["trigger_right"] // 1)))
+                # window.Element('_0-2_').Update(
+                #     '{}            {}            {}'.format((telem.get("stick_left_x") // 1),
+                #                                             (telem.get("stick_right_x") // 1),
+                #                                             (telem.get("trigger_left") // 1)))
+                # window.Element('_3-5_').Update(
+                #     '{}            {}            {}'.format((telem["stick_left_y"] // 1), (telem["stick_right_y"] // 1),
+                #                                             (telem["trigger_right"] // 1)))
+                window.Element('left').Update(
+                    'Stick Left X: \n{}\nStick Left Y: \n{}\nLeft Trigger: \n{}\n'.format((telem.get("stick_left_x") // 1),
+                                                            (telem.get("stick_left_y") // 1),
+                                                            (telem.get("trigger_left") // 1)))
+                window.Element('right').Update(
+                    'Stick Right X: \n{}\nStick Right Y: \n{}\nRight Trigger: \n{}\n'.format(
+                        (telem.get("stick_right_x") // 1),
+                        (telem.get("stick_right_y") // 1),
+                        (telem.get("trigger_right") // 1)))
             refresh_rate = ((values[0] - 50) ** 2 + 1)
             telem = self.get_data()
             # print("My bs: ", telem)
