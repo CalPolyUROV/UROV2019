b5a094281b4a7d77d1c78a474ce2b9dee990ef98
Spencer Shaw <sfshaw@calpoly.edu>
2020-02-28 00:16:00 -0800

    Clean up GUI endpoint

--- raspi/ui/gui/gui_endpoint.py
+++ raspi/ui/gui/gui_endpoint.py
@@ -63,6 +63,8 @@ class SimpleGUI(AsyncEndpoint):
     def update_gui(self):
         # Get updated telemetry data
         # TODO: Use page freshness abstraction to check if data is unchanged
+        # For now, get data returns the specific data and not a Page
+        #  object that has freshness
         data = self.get_data()
         # Please try and use as high of a timeout value as you can
         event, values = self.window.Read(timeout=self.refresh_rate)
@@ -70,23 +72,28 @@ class SimpleGUI(AsyncEndpoint):
         if event is None or event == 'Quit':
             self.close_btn_action()
         if settings.GUI_channels["controller"]:
-            self.dbg("gui_control", "Got controler info: {}", [data[0]])
-            if data[0] is not None and data[0].get("stick_left_x") is not None:
-                self.window.Element('left').Update(
-                    'Stick Left X: \n{}\nStick Left Y: \n{}\nLeft Trigger: \n{}\n'.format(
-                        (data[0].get("stick_left_x") // 1),
-                        (data[0].get("stick_left_y") // 1),
-                        (data[0].get("trigger_left") // 1)))
-                self.window.Element('right').Update(
-                    'Stick Right X: \n{}\nStick Right Y: \n{}\nRight Trigger: \n{}\n'.format(
-                        (data[0].get("stick_right_x") // 1),
-                        (data[0].get("stick_right_y") // 1),
-                        (data[0].get("trigger_right") // 1)))
+            self.update_controller_pane(data)
         if settings.GUI_channels["telem"]:
             self.dbg("gui_telem", "Got telem info: {}", [data[1]])
         # Update refresh rate from GUI
         self.dbg("gui_verbose", "UI tick_rate value: {}", [values[0]])
         self.set_refresh_rate(values[0])
+
+    def update_controller_pane(self, data):
+        self.dbg("gui_control", "Got controler info: {}", [data[0]])
+        if data[0] is not None and data[0].get("stick_left_x") is not None:
+            col_l = "Stick Left X: \n{}\nStick Left Y: \n{}\nLeft Trigger: \n{}\n"
+            self.window.Element('left').Update(
+                col_l.format(
+                    (data[0].get("stick_left_x") // 1),
+                    (data[0].get("stick_left_y") // 1),
+                    (data[0].get("trigger_left") // 1)))
+            col_r = "Stick Right X: \n{}\nStick Right Y: \n{}\nRight Trigger: \n{}\n"
+            self.window.Element('right').Update(
+                col_r.format(
+                    (data[0].get("stick_right_x") // 1),
+                    (data[0].get("stick_right_y") // 1),
+                    (data[0].get("trigger_right") // 1)))
 
     def set_refresh_rate(self, rate):
         self.dbg("gui_verbose",
@@ -101,18 +108,17 @@ class SimpleGUI(AsyncEndpoint):
         layout = []
         if settings.GUI_channels["controller"]:
             layout += [[sg.Frame(layout=[
-                [
+                [sg.Text(
+                    'Stick Left X: \n0\nStick Left Y: \n0\nLeft Trigger: \n0\n',
+                    size=(20, 6),
+                    justification='center',
+                    key='left'),
                     sg.Text(
-                        'Stick Left X: \n0\nStick Left Y: \n0\nLeft Trigger: \n0\n',
-                        size=(20, 6),
-                        justification='center',
-                        key='left'),
-                    sg.Text(
-                        'Stick Right X: \n0\nStick Right Y: \n0\nRight Trigger: \n0\n',
-                        size=(20, 6),
-                        justification='center',
-                        key='right')
-                ]],
+                    'Stick Right X: \n0\nStick Right Y: \n0\nRight Trigger: \n0\n',
+                    size=(20, 6),
+                    justification='center',
+                    key='right')
+                 ]],
                 title='Control Data',
                 title_color='Black',
                 relief=sg.RELIEF_SUNKEN)]]
